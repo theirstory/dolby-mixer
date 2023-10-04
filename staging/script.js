86b9c00db9ca5f0a1009e5d26557b381f4770556
JuanPortaEureka <129301928+JuanPortaEureka@users.noreply.github.com>
2023-10-04 16:56:53 -0300

    Update script.js

--- staging/script.js
+++ staging/script.js
@@ -133,6 +133,7 @@ const resizeVideos = () => {
     } else {
       const node = $("#participant-" + p);
       node.css("width", `${newWidth}px`);
+      node.css("height", `${newHeight}px`);
       node.find("video").css({"height": `${newHeight}px`, "object-fit": `contain`});
     }
   });
